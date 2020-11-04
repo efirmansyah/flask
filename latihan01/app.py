fe7b45a09e48589e23381c3af6c3ad1f2604efda
efirmansyah <endarfirmansyah@gmail.com>
2020-11-04 14:49:08 +0700

    Flask Tutorial

--- latihan01/app.py
+++ latihan01/app.py
@@ -5,13 +5,13 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, World, Dilo Bandung'
 
-@app.route('/index')
-def index():
-    return 'Index Page'
+@app.route('/home')
+def home():
+    return 'This is home page'
 
-@app.route('/hello')
-def hello():
-    return 'Hello, World'
+@app.route('/about')
+def about():
+    return 'This is hello page'
 
 @app.route('/hi')
 def hi():
@@ -22,7 +22,10 @@ def show_user_profile(username):
     # show the user profile for that user
     return 'User %s' % username
 
-
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    # show the post with the given id, the id is an integer
+    return 'Post %d' % post_id
 
 
 
